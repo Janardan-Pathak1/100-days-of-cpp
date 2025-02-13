aa16efdfa36ddb1a0d097e6a440ec434aa739f98
0xt3uk1 <janardanpathak7777@gmail.com>
2025-02-13 20:29:40 +0530

    added codes

---
+++ Day_2/Demo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std; // means that we can use names for objects and variables
+                     // from the standard library.
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
---
+++ Day_2/demo2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World " << endl;
+  cout << "I am here to help!";
+  return 0;
+}
