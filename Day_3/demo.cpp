a55b19a248bd7603daf3fc6df5a7a1f4f25db7b7
0xt3uk1 <janardanpathak7777@gmail.com>
2025-02-14 19:38:42 +0530

    DAY 3

---
+++ Day_3/demo.cpp
@@ -0,0 +1,15 @@
+// Practicing cout and cin
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x, y;
+  cout << "Enter first number: ";
+  cin >> x;
+  cout << "Enter second number: ";
+  cin >> y;
+  cout << "Sum of " << x << " " << y << "is =" << x + y;
+  return 0;
+}
