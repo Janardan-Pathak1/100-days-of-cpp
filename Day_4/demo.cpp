cdc0a5caeb2b5a855cb0491deb3c88954cb90807
0xt3uk1 <janardanpathak7777@gmail.com>
2025-02-15 09:23:05 +0530

    DAY 4

---
+++ Day_4/demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x = 5; // Assignmet operator
+  x = x | 3;
+  int a = 6;
+  cout << (x == a) << endl;
+  cout << x;
+
+  return 0;
+}
