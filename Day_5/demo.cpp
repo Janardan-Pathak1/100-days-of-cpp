458043ffc599febdc4360ff2fcdbff906333b5e8
0xt3uk1 <janardanpathak7777@gmail.com>
2025-02-16 21:09:04 +0530

    DAY 5

---
+++ Day_5/demo.cpp
@@ -0,0 +1,19 @@
+// COntrol Statements
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int myNum = 10; // Is this a positive or negative number?
+
+  if (myNum > 0) {
+    cout << "The value is a positive number.\n";
+  } else if (myNum < 0) {
+    cout << "The value is a negative number.\n";
+  } else {
+    cout << "The value is 0.\n";
+  }
+
+  return 0;
+}
