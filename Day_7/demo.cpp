a392c31daac71e641ad4bc25f5ecffa4d0472731
0xt3uk1 <janardanpathak7777@gmail.com>
2025-02-21 13:08:40 +0530

    DAY 7

---
+++ Day_7/demo.cpp
@@ -0,0 +1,14 @@
+// Basics of Functions in c++
+
+#include <iostream>
+
+using namespace std;
+void test() { // declaration of a function
+  cout << "This is the test function";
+}
+int main() {
+  test(); // caliing the function
+  test();
+  test();
+  return 0;
+}
